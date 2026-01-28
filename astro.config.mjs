83964032d6ece05a33938c8d2f072c92f9963dd0
av1-lv5 <90096511+Av1-Lv5@users.noreply.github.com>
2026-01-28 16:09:22 +0530

    feat: integrate rehype plugin for external links in markdown to open in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from 'rehype-external-links';
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, {
+        target: '_blank',
+        rel: ['noopener', 'noreferrer']
+      }]
+    ]
+  },
   integrations: [icon()],
   site: "https://alvs.dev"
 });
